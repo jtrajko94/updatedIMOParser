0df1f892c1f42a7f144b68763ebf5511400abae9
Jerun Trajko <trajko2@illinois.edu>
2016-03-14 14:29:35 -0500

    done

---
+++ ExcelParser/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb; //Read excel file as a databases
+
+namespace ExcelToAccess
+{
+    public partial class Form1 : Form
+    {
+        OleDbConnection con;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dgShow.DataSource = null;
+            dgShow.Refresh();
+
+            //Set up the connection and put it in the data grid
+            OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source="+ txtFileName.Text +";Extended Properties='Excel 8.0;HDR=YES;IMEX=1;';");
+            OleDbDataAdapter da = new OleDbDataAdapter("select * from [Export Worksheet$]", con);
+            da.Fill(dsInfo);
+            dgShow.DataSource = dsInfo.Tables[0];
+
+            //Query the information from the worksheet
+            string selectQuery = "SELECT * from [Export Worksheet$]";
+            OleDbCommand myCommand = new OleDbCommand(selectQuery, con);
+            myCommand.Connection.Open();
+
+            //Read through each row and store all the person objects into an array
+            var peopleResults = new List<Person>();
+            OleDbDataReader myReader = myCommand.ExecuteReader(CommandBehavior.CloseConnection);
+
+            Client cli = new Client();
+            cli.initClient();
+            while(myReader.Read())
+            {
+                Person person = new Person();
+                if (myReader[@"PERSON$_CODE"] != DBNull.Value)
+                {
+                    person.PERSON_CODE = (double)myReader[@"PERSON$_CODE"];
+                }
+                if (myReader["FIRST_NAME"] != DBNull.Value)
+                {
+                    person.FIRST_NAME = (string)myReader["FIRST_NAME"];
+                }
+                if(myReader["LAST_NAME"] != DBNull.Value)
+                {
+                    person.LAST_NAME = (string)myReader["LAST_NAME"];
+                }
+                if(myReader["DOB"] != DBNull.Value)
+                {
+                    person.DOB = (string)myReader["DOB"];
+                }
+                if(myReader["ADDRESS_1"] != DBNull.Value)
+                {
+                    person.ADDRESS_1 = (string)myReader["ADDRESS_1"];
+                }
+                if (myReader["ADDRESS_2"] != DBNull.Value)
+                {
+                    person.ADDRESS_2 = (string)myReader["ADDRESS_2"];
+                }
+                if (myReader["ADDRESS_TYPE"] != DBNull.Value)
+                {
+                    person.ADDRESS_TYPE = (string)myReader["ADDRESS_TYPE"];
+                }
+                if (myReader["CITY"] != DBNull.Value)
+                {
+                    person.CITY = (string)myReader["CITY"];
+                }
+                if (myReader["STATE"] != DBNull.Value)
+                {
+                    person.STATE = (string)myReader["STATE"];
+                }
+                if (myReader["ZIP_CODE"] != DBNull.Value)
+                {
+                    person.ZIP_CODE = (string)myReader["ZIP_CODE"];
+                }
+                if (myReader["EMAIL"] != DBNull.Value)
+                {
+                    person.EMAIL = (string)myReader["EMAIL"];
+                }
+                if (myReader["TELEPHONE_1"] != DBNull.Value)
+                {
+                    person.TELEPHONE_1 = (string)myReader["TELEPHONE_1"];
+                }
+                if (myReader["ADMIT_DATE"] != DBNull.Value)
+                {
+                    person.ADMIT_DATE = (string)myReader["ADMIT_DATE"];
+                }
+                peopleResults.Add(person);
+            }
+            cli.uploadPatients(peopleResults);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnReadMedication_Click(object sender, EventArgs e)
+        {
+            dgShow.DataSource = null;
+            dgShow.Refresh();
+            //Set up the connection and put it in the data grid
+            OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source="+ txtFileName.Text +";Extended Properties='Excel 8.0;HDR=YES;IMEX=1;';");
+            OleDbDataAdapter da = new OleDbDataAdapter("select * from [Export Worksheet$]", con);
+            da.Fill(dsInfo);
+            dgShow.DataSource = dsInfo.Tables[0];
+
+            //Query the information from the worksheet
+            string selectQuery = "SELECT * from [Export Worksheet$]";
+            OleDbCommand myCommand = new OleDbCommand(selectQuery, con);
+            myCommand.Connection.Open();
+
+            //Read through each row and store all the person objects into an array
+            var medicationResults = new List<excelMedication>();
+            OleDbDataReader myReader = myCommand.ExecuteReader(CommandBehavior.CloseConnection);
+            while (myReader.Read())
+            {
+                excelMedication medication = new excelMedication();
+                if (myReader[@"PERSON$_CODE"] != DBNull.Value)
+                {
+                    medication.PERSON_CODE = (double)myReader[@"PERSON$_CODE"];
+                }
+                if (myReader["FIRST_NAME"] != DBNull.Value)
+                {
+                    medication.FIRST_NAME = (string)myReader["FIRST_NAME"];
+                }
+                if (myReader["LAST_NAME"] != DBNull.Value)
+                {
+                    medication.LAST_NAME = (string)myReader["LAST_NAME"];
+                }
+                if (myReader["DOB"] != DBNull.Value)
+                {
+                    medication.DOB = (string)myReader["DOB"];
+                }
+                if (myReader["MED_NAME"] != DBNull.Value)
+                {
+                    medication.MED_NAME = (string)myReader["MED_NAME"];
+                }
+                if (myReader[@"MEDICATIONS$_TEXT"] != DBNull.Value)
+                {
+                   medication.MEDICATION_TEXT = (string)myReader[@"MEDICATIONS$_TEXT"];
+                }
+                medicationResults.Add(medication);
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            xxx2fhir.json2Resource(txtFileName.Text);
+        }
+    }
+
+  
+}
---
+++ ExcelParser/fhir2sql.cs
@@ -0,0 +1,45 @@
+﻿using Hl7.Fhir.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelToAccess
+{
+    class fhir2sql
+    {
+        public static sqlPatient prepareImportPatient(Patient patient, string consentDate,string population)
+        {
+            var sqlP = new sqlPatient();
+            sqlP.FIRST_NAME = patient.Name.ElementAt(0).Given.First();
+            sqlP.LAST_NAME = patient.Name.ElementAt(0).Given.Last();
+            sqlP.GENDER = patient.Gender.Value.ToString();
+            sqlP.ADDRESS = patient.Address.ElementAt(0).Text;
+            sqlP.STATE_OR_PROVINCE_CODE = patient.Address.ElementAt(0).State;
+            sqlP.POSTAL_CODE = patient.Address.ElementAt(0).PostalCode;
+            sqlP.CITY = patient.Address.ElementAt(0).City;
+            sqlP.CONSENT_DATE = consentDate;
+            sqlP.POPULATION = population;
+            return sqlP;
+        }
+        /*
+        public string[] prepareCreatePopulation(string population, string IRB_approve, string IRB_expire, string min_trigger, string max_trigger,string no_connect_trigger)
+        {
+            return new string[6] { population, IRB_approve, IRB_expire, min_trigger, max_trigger, no_connect_trigger };
+        }
+        public string[] prepareAssignPatientPopulation(string patientID, string SiteID,string populationID,string consentdate)
+        {
+            return new string[4] { patientID, SiteID, populationID, consentdate};
+        }
+
+        public string[] prepareImportPrescription(MedicationOrder mo, string population, )
+        {
+            ResourceReference medRef = (ResourceReference) mo.Medication;
+            string reference = medRef.Reference;
+        }
+    }
+    */
+    }
+
+}
